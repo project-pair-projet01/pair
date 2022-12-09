afb2534d82dbd8029f1fb634d9dff49887e4daed
Haythem Krifi <117401541+Hk11234@users.noreply.github.com>
2022-12-09 10:34:12 +0100

    Add files via upload

---
+++ paire-project.js
@@ -0,0 +1,18 @@
+
+
+// function myFunction() {
+//     alert("Check your printer please");
+//   }
+
+
+  $(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideUp("slow");
+    });
+  });
